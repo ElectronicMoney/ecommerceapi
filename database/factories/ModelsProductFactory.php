99dbb8985cdae9774192870aa8d5ed87a570a404
Emeka Augustine <cafafanscoders@gamil.com>
2019-01-07 03:23:55 -0500

    Added Seeding part of product and review

--- database/factories/ModelsProductFactory.php
+++ database/factories/ModelsProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+        'price' => $faker->numberBetween(100, 1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2, 30)
     ];
 });
--- database/factories/ModelsReviewFactory.php
+++ database/factories/ModelsReviewFactory.php
@@ -1,9 +1,13 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Models\Product as Product;
 
 $factory->define(App\Models\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => Product::all()->random(),
+        'customer' => $faker->name,
+        'review' => $faker->paragraph($nbSentences = 2, $variableNbSentences = true),
+        'star' => $faker->numberBetween(0, 5)
     ];
 });
