b807f5faaaeb0fdecd9a67d7f19b1d00b38f5c4f
Emeka Augustine <cafafanscoders@gamil.com>
2019-01-07 14:46:59 -0500

    Relationship created for Product and Review Models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    protected $fillable = [
+        'name', 'detail', 'price', 'stock', 'discount'
+    ];
+
+    public function review() {
+        return $this->hasMany('App\Models\Review');
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     //
+    protected $fillable = [
+        'customer', 'review', 'star'
+    ];
+    public function product() {
+        return $this->belongsTo('App\Models\Product');
+    }
 }
